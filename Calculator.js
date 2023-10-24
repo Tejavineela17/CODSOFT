d250f506ca609c3e8f6e64558ad582fd21204cf0
Tejavineela17 <148855727+Tejavineela17@users.noreply.github.com>
2023-10-24 15:31:49 +0530

    Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,17 +1,17 @@
-function display(val) {
-   var v = document.getElementById('displaybox');
+function Solve(val) {
+   var v = document.getElementById('res');
    v.value += val;
 }
 function Result() {
-   var num1 = document.getElementById('displaybox').value;
+   var num1 = document.getElementById('res').value;
    var num2 = eval(num1);
-   document.getElementById('displaybox').value = num2;
+   document.getElementById('res').value = num2;
 }
-function clear() {
-   var inp = document.getElementById('displaybox');
+function Clear() {
+   var inp = document.getElementById('res');
    inp.value = '';
 }
-function back() {
-   var ev = document.getElementById('displaybox');
+function Back() {
+   var ev = document.getElementById('res');
    ev.value = ev.value.slice(0,-1);
 }
